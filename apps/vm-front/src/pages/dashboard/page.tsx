3b0f309d1d11648fe7f1b322ed4d66fb56b0e101
DianHuiza <dianhuiza@gmail.com>
2024-10-11 16:32:02 -0400

    test: test options

--- apps/vm-front/src/pages/dashboard/page.tsx
+++ apps/vm-front/src/pages/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+
 export const Component = () => {
-  return <div className=' flex items-center justify-center h-screen'>Dashboard</div>
+  const {data} = useQuery({
+    queryKey: ['api'],
+    queryFn: async ()=> {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api`)
+      return await res.json()
+    }
+  })
+  return <div className=' flex items-center justify-center h-screen'>{data?.message ?? 'Loading...'}</div>
 }
