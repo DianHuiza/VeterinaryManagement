413964efcea2182bf8050110e2b42e279a4ef908
DianHuiza <dianhuiza@gmail.com>
2024-10-16 02:18:02 -0400

    feat(vm-back):add zop pipe

---
+++ apps/vm-back/src/pipes/zod.pipe.ts
@@ -0,0 +1,19 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema<any>) {}
+
+  transform(value: any) {
+    let validated;
+    try {
+      validated = this.schema.safeParse(value);
+    } catch (err: unknown) {
+      if (err instanceof ZodError) {
+        throw new BadRequestException(err.issues[0].message);
+      }
+    }
+    return validated;
+  }
+}
