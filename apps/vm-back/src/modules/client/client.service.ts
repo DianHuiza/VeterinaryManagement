4049a4f4843135516f2bd62afcc9bf2c6dec696c
DianHuiza <dianhuiza@gmail.com>
2024-10-16 16:26:12 -0400

    feat(vm-back): Create Client Endpoints

--- apps/vm-back/src/modules/client/client.service.ts
+++ apps/vm-back/src/modules/client/client.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+  constructor(private prisma: PrismaService) {}
+  async getAllClients(page: number, includeDeleted: boolean) {
+    return this.prisma.client.findMany({
+      skip: page * 15,
+      take: 15,
+      where: {
+        deletedAt: includeDeleted ? undefined : { not: null },
+      },
+    });
+  }
+
+  async getClientById(id: string) {
+    return this.prisma.client.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createClient(client: any) {
+    return this.prisma.client.create({
+      data: client,
+    });
+  }
+
+  async updateClient(client: any) {
+    return this.prisma.client.update({
+      where: {
+        id: client.id,
+      },
+      data: client,
+    });
+  }
+
+  async deleteClient(id: string) {
+    return this.prisma.client.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async softDeleteClient(id: string) {
+    return this.prisma.client.update({
+      where: {
+        id,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+}
