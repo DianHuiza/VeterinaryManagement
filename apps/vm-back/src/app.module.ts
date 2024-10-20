07da6b7355ff0d8a0e1ecb50a9b39eaf4aaa1131
DianHuiza <dianhuiza@gmail.com>
2024-10-20 15:57:33 -0400

    feat(vm-back): Create file module

--- apps/vm-back/src/app.module.ts
+++ apps/vm-back/src/app.module.ts
@@ -5,9 +5,10 @@ import { ClientModule } from './modules/client/client.module';
 import { EmployeeModule } from './modules/employee/employee.module';
 import { RoomModule } from './modules/room/room.module';
 import { ServicesModule } from './modules/services/services.module';
+import { FilesModule } from './modules/files/files.module';
 
 @Module({
-  imports: [ClientModule, EmployeeModule, RoomModule, ServicesModule],
+  imports: [ClientModule, EmployeeModule, RoomModule, ServicesModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
