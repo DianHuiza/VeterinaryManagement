b2300f713d61ee77c1c5e0933cbe5624f9bba58f
DianHuiza <dianhuiza@gmail.com>
2024-10-24 16:52:37 -0400

    feat(vm-back): Finish option module

--- apps/vm-back/src/modules/options/options.controller.ts
+++ apps/vm-back/src/modules/options/options.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OptionsService } from './options.service';
+import { UpdateOptionDTO } from './dto/options_zod';
 
 @Controller('options')
 export class OptionsController {
   constructor(private readonly optionsService: OptionsService) {}
+  @Get()
+  getOptions() {
+    return this.optionsService.getOptions();
+  }
+  @Post()
+  setOption(@Body() option: UpdateOptionDTO) {
+    return this.optionsService.setOption(option);
+  }
 }
